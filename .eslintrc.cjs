ca753e21a861e02e934681ed81d33465428e97b1
Nguyen Minh Anh <xiaosasori@gmail.com>
2023-08-07 21:09:32 +0900

    init

---
+++ .eslintrc.cjs
@@ -0,0 +1,13 @@
+module.exports = {
+  root: true,
+  extends: [
+    '@antfu',
+    // 'plugin:vue/vue3-essential',
+    // 'eslint:recommended',
+    // '@vue/eslint-config-typescript',
+    '@vue/eslint-config-prettier/skip-formatting'
+  ],
+  parserOptions: {
+    ecmaVersion: 'latest'
+  }
+}
