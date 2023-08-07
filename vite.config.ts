ab5bd66303e22343a945c380ac20b4277b94ed78
Nguyen Minh Anh <xiaosasori@gmail.com>
2023-08-07 21:20:27 +0900

    add unplugin-fonts

--- vite.config.ts
+++ vite.config.ts
@@ -1,12 +1,20 @@
-import { fileURLToPath, URL } from 'node:url'
+import { URL, fileURLToPath } from 'node:url'
 
 import { defineConfig } from 'vite'
 import vue from '@vitejs/plugin-vue'
+import Unfonts from 'unplugin-fonts/vite'
 
 // https://vitejs.dev/config/
 export default defineConfig({
   plugins: [
     vue(),
+    Unfonts({
+      google: {
+        families: [
+          { name: 'Quicksand' }
+        ]
+      }
+    }),
   ],
   resolve: {
     alias: {
