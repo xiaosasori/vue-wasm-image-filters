4223b3c50be96c718679476d30e616bdea730a3d
Nguyen Minh Anh <xiaosasori@gmail.com>
2023-08-10 09:19:32 +0900

    enable WebAssembly in Vite

--- vite.config.ts
+++ vite.config.ts
@@ -5,19 +5,26 @@ import vue from '@vitejs/plugin-vue'
 import Unfonts from 'unplugin-fonts/vite'
 import Components from 'unplugin-vue-components/vite'
 
+import wasm from 'vite-plugin-wasm'
+import topLevelAwait from 'vite-plugin-top-level-await'
+
 // https://vitejs.dev/config/
 export default defineConfig({
   plugins: [
     vue(),
     Unfonts({
       google: {
-        families: [
-          { name: 'Quicksand' }
-        ]
+        families: [{ name: 'Quicksand' }]
       }
     }),
     Components(),
+    wasm(),
+    topLevelAwait()
   ],
+  optimizeDeps: {
+    // exlude wasm file
+    exclude: ['@silvia-odwyer/photon']
+  },
   resolve: {
     alias: {
       '@': fileURLToPath(new URL('./src', import.meta.url))
